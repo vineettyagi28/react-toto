5a6ae23b1af6dc0bd970adc648a5311eebb4c64b
Vineet Tyagi <Vineet.Tyagi@iontrading.com>
2017-08-29 12:23:09 +0530

    Added tests

---
+++ app/tests/TodoApp.test.jsx
@@ -0,0 +1,27 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var TodoApp = require('TodoApp');
+
+describe('TodoApp', () => {
+  it('should exist', () => {
+    expect(TodoApp).toExist();
+  });
+
+  it('should render one Todo component for each todo item', () => {
+    var todos = [{
+      id: 1,
+      text: 'Do something'
+    }, {
+      id: 2,
+      text: 'Check mail'
+    }];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
+  });
+});
